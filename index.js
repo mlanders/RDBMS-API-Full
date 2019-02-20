ed78a784b46a51c36ab79a04a8b8d27ffe8d8573
Mike Landers <landers.mike@gmail.com>
2019-02-19 16:40:32 -0800

    Stretch completed

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ server.put('/api/cohorts/:id', async (req, res) => {
 		const result = await db('cohorts')
 			.where('id', id)
 			.update({ name });
-		res.status(201).json({ message: 'Successfully updated the cohort', result });
+		res.status(200).json({ message: 'Successfully updated the cohort', result });
 	} catch (error) {
 		res.status(500).json({ message: 'Unable to update the cohort' });
 	}
@@ -86,6 +86,75 @@ server.delete('/api/cohorts/:id', async (req, res) => {
 		res.status(201).json({ message: 'Successfully deleted the cohort', result });
 	} catch (error) {
 		res.status(500).json({ message: 'Unable to delete the cohort' });
+	}
+});
+
+/// ***** STRETCH *****
+server.get('/api/students', async (req, res) => {
+	try {
+		const result = await db('students');
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+server.get('/api/students/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const studentCheck = await db('students').where('id', id);
+		if (studentCheck.length) {
+			const result = await db('students').where('id', id);
+			res.status(200).json(result);
+		} else {
+			res.status(440).json({ message: 'Unable to find a student with that ID' });
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+server.post('/api/students', async (req, res) => {
+	try {
+		const { name, cohort_id } = req.body;
+		if (!name) {
+			res.status(404).json({ message: 'Please include both name and cohort ID' });
+		} else {
+			const result = await db('students').insert({ name, cohort_id });
+			res.status(201).json({ message: 'Successfully added the student', result });
+		}
+	} catch (error) {
+		res.status(500).json({ message: 'Unable to add the student' });
+	}
+});
+
+server.put('/api/students/:id', async (req, res) => {
+	try {
+		const { name, cohort_id } = req.body;
+		const { id } = req.params;
+		const studentCheck = await db('students').where('id', id);
+		if (studentCheck.length) {
+			const result = await db('students')
+				.where('id', id)
+				.update({ name, cohort_id });
+			res.status(200).json({ message: 'Successfully updated the student', result });
+		} else {
+			res.status(404).json({ message: 'Unable to update the student record' });
+		}
+	} catch (error) {
+		res.status(500).json({ message: 'Unable to update the student' });
+	}
+});
+
+server.delete('/api/students/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = await db('students')
+			.where('id', id)
+			.del();
+		res.status(204).json({ message: 'Successfully deleted the student', result });
+	} catch (error) {
+		res.status(500).json({ message: 'Unable to delete the student' });
 	}
 });
 
